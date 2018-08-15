d874b658b9b96e276e24c5f04f3400e75679e4e0
Parker Smith <parker@parkersmith.us>
2018-08-14 20:32:07 -0600

    Add selenium get to parse javascript before returning html

---
+++ selenium_get.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+def simple_get(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    chrome_driver = os.getcwd() + "/chromedriver"
+
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    driver.get(url)
+    
+    raw_html = driver.execute_script("return document.body.innerHTML")
+
+    return raw_html
