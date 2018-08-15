f2faecde32cd5f41d7c9b213ba93fad1d643ea23
Parker Smith <parker@parkersmith.us>
2018-08-14 20:32:34 -0600

    Use selenium get module

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
-from simple_get import simple_get
+from selenium_get import simple_get
 from bs4 import BeautifulSoup
 
 raw_html = simple_get('https://classifieds.ksl.com')
 html = BeautifulSoup(raw_html, 'html.parser')
-new_items = html.find_all('div', class_="page-wrap")
+new_items = html.find_all(class_="newestListings-listingTitle")
 
 print(new_items)
