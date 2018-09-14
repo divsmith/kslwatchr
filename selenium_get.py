87c94b4c62d5be5bf35f3d1010a75bbdca308fae
Jan Taylor <janttaylor@gmail.com>
2018-09-13 21:09:50 -0600

    Update Readme and make selenium work for Windows

--- selenium_get.py
+++ selenium_get.py
@@ -8,8 +8,9 @@ def simple_get(url):
     chrome_options.add_argument("--window-size=1920x1080")
 
     chrome_driver = os.getcwd() + "/chromedriver"
+    chrome_driver = os.getcwd() + "/chromedriver.exe"
 
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver or chrome_driver_win)
     driver.get(url)
     
     raw_html = driver.execute_script("return document.body.innerHTML")
