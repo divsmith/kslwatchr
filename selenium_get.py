169b126cb5762f2fe5745911f1565c640c565bfc
Jan Taylor <janttaylor@gmail.com>
2018-09-13 21:36:56 -0600

    Change it to run chromedriver on 'nt' instead

--- selenium_get.py
+++ selenium_get.py
@@ -6,9 +6,8 @@ def simple_get(url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920x1080")
-
-    chrome_driver = os.getcwd() + ("chromedriver" if os.name == 'Linux' else "/chromedriver.exe")
-
+    chrome_driver = os.getcwd() + ("/chromedriver.exe" if os.name == 'nt' else "/chromedriver")
+    
     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
 
     driver.get(url)
