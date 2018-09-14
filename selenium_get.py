c66bac9fa2887f28ccb1dd83a0b45c85c6d9a45e
Jan Taylor <janttaylor@gmail.com>
2018-09-13 21:32:27 -0600

    Ternary for os name and some gitignore changes

--- selenium_get.py
+++ selenium_get.py
@@ -7,10 +7,10 @@ def simple_get(url):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920x1080")
 
-    chrome_driver = os.getcwd() + "/chromedriver"
-    chrome_driver = os.getcwd() + "/chromedriver.exe"
+    chrome_driver = os.getcwd() + ("chromedriver" if os.name == 'Linux' else "/chromedriver.exe")
 
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver or chrome_driver_win)
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+
     driver.get(url)
     
     raw_html = driver.execute_script("return document.body.innerHTML")
